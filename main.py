4a8c05525d3ed272c66ce42dcd1ef32f119ae2e0
Seongbum Seo <seo@seongbum.com>
2023-08-11 16:36:46 +0900

    Add ChatGPT API

--- main.py
+++ main.py
@@ -2,26 +2,47 @@ import fire
 import gradio as gr
 import torch
 
-from alpaca.core import Alpaca
+def get_device():
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
 
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
+    try:
+        if torch.backends.mps.is_available():
+            device = "mps"
+    except:  # noqa: E722
+        pass
+    return device
 
-try:
-    if torch.backends.mps.is_available():
-        device = "mps"
-except:  # noqa: E722
-    pass
+def run_chatgpt(
+    openai_api_key: str = "",
+    server_name: str = "0.0.0.0",
+    server_port: int = 36000,
+    share_gradio: bool = False,
+):
+    from chatgpt.core import ChatGPT
+    chatgpt = ChatGPT(api_key=openai_api_key)
+    
+    gr.Interface(
+        fn=chatgpt.fn,
+        inputs=chatgpt.inputs,
+        outputs=chatgpt.outputs,
+        title="MAIA (ChatGPT Only)",
+    ).queue().launch(
+        server_name=server_name,
+        server_port=server_port,
+        share=share_gradio
+    )
 
 def main(
     server_name: str = "0.0.0.0",
     server_port: int = 36000,
     share_gradio: bool = False,
 ):
+    from alpaca.core import Alpaca
     alpaca = Alpaca(
-        device=device,
+        device=get_device(),
         load_8bit=True,
         base_model="decapoda-research/llama-7b-hf",
         lora_weights="tloen/alpaca-lora-7b",
