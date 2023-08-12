c6376d420fdee6a8118ffaf0f91c5b681ef81187
Seongbum Seo <seo@seongbum.com>
2023-08-13 05:35:27 +0900

    Add LM-STT pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import fire
 import gradio as gr
 import torch
+from typing import Callable
 
 def get_device():
     if torch.cuda.is_available():
@@ -45,7 +46,7 @@ def run_whisperx(
     
     whisper = WhisperX(
         device=get_device(),
-        device_index= 0,
+        device_index=0,
         compute_type="float16",
         batch_size=16,
     )
@@ -54,14 +55,14 @@ def run_whisperx(
         fn=whisper.fn,
         inputs=whisper.inputs,
         outputs=whisper.outputs,
-        title="MAIA",
+        title="MAIA (WhisperX Only)",
     ).queue().launch(
         server_name=server_name,
         server_port=server_port,
-        share=share_gradio
+        share=share_gradio,
     )
 
-def main(
+def run_alpaca(
     server_name: str = "0.0.0.0",
     server_port: int = 36000,
     share_gradio: bool = False,
@@ -79,11 +80,54 @@ def main(
         fn=alpaca.fn,
         inputs=alpaca.inputs,
         outputs=alpaca.outputs,
+        title="MAIA (Alpaca Only)",
+    ).queue().launch(
+        server_name=server_name,
+        server_port=server_port,
+        share=share_gradio,
+    )
+
+def main(
+    server_name: str = "0.0.0.0",
+    server_port: int = 36000,
+    share_gradio: bool = False,
+):
+    from whisperx.core import WhisperX
+    from alpaca.core import Alpaca
+    
+    whisper = WhisperX(
+        device=get_device(),
+        device_index=0,
+        compute_type="float32",
+        batch_size=16,
+    )
+    
+    alpaca = Alpaca(
+        device=get_device(),
+        load_8bit=True,
+        base_model="decapoda-research/llama-7b-hf",
+        lora_weights="tloen/alpaca-lora-7b",
+    )
+
+    def pipeline(
+        transcribe: Callable[..., str],
+        generate: Callable[..., str],
+    ):
+        def inference(*args, **kwargs):
+            transcript = transcribe(*args, **kwargs)
+            response = "".join(generate(transcript))
+            return response
+        return inference
+
+    gr.Interface(
+        fn=pipeline(whisper.fn, alpaca.fn),
+        inputs=whisper.inputs,
+        outputs=alpaca.outputs,
         title="MAIA",
     ).queue().launch(
         server_name=server_name,
         server_port=server_port,
-        share=share_gradio
+        share=share_gradio,
     )
 
 if __name__ == "__main__":
